6c9cc4b36407d42c95b5b48ad9f9ef6f515d0880
ChenKefan <ckf_hclo4@outlook.com>
2024-05-09 12:38:00 +0800

    fixed the dir of gain_collection in dataParse.py

--- utils/dataParse.py
+++ utils/dataParse.py
@@ -80,16 +80,16 @@ def get_dataset(dataset):
     print('loading', dataset)
 
     if dataset == 'mimic27':
-        x = np.array(pd.read_csv('../gain_collection/collected_gain/mimic27/mimic_x.csv', header=None, sep=' '))
-        y = np.array(pd.read_csv('../gain_collection/collected_gain/mimic27/mimic_y_valid.csv', header=None, sep=' '))
-        testx = np.array(pd.read_csv('../gain_collection/collected_gain/mimic27/mimic_x.csv', header=None, sep=' '))
+        x = np.array(pd.read_csv('gain_collection/collected_gain/mimic27/mimic_x.csv', header=None, sep=' '))
+        y = np.array(pd.read_csv('gain_collection/collected_gain/mimic27/mimic_y_valid.csv', header=None, sep=' '))
+        testx = np.array(pd.read_csv('gain_collection/collected_gain/mimic27/mimic_x.csv', header=None, sep=' '))
         testy = np.array(
-            pd.read_csv('../gain_collection/collected_gain/mimic27/mimic_y_test.csv', header=None, sep=' '))
+            pd.read_csv('gain_collection/collected_gain/mimic27/mimic_y_test.csv', header=None, sep=' '))
     elif dataset == 'taskonomy':
-        x = np.array(pd.read_csv('../gain_collection/collected_gain/taskonomy/taskonomy_x.csv', header=None))
-        y = np.array(pd.read_csv('../gain_collection/collected_gain/taskonomy/taskonomy.csv', sep=' ', header=None))
-        testx = np.array(pd.read_csv('../gain_collection/collected_gain/taskonomy/taskonomy_x.csv', header=None))
-        testy = np.array(pd.read_csv('../gain_collection/collected_gain/taskonomy/taskonomy.csv', sep=' ', header=None))
+        x = np.array(pd.read_csv('gain_collection/collected_gain/taskonomy/taskonomy_x.csv', header=None))
+        y = np.array(pd.read_csv('gain_collection/collected_gain/taskonomy/taskonomy.csv', sep=' ', header=None))
+        testx = np.array(pd.read_csv('gain_collection/collected_gain/taskonomy/taskonomy_x.csv', header=None))
+        testy = np.array(pd.read_csv('gain_collection/collected_gain/taskonomy/taskonomy.csv', sep=' ', header=None))
     else:
         raise CustomError('Cannot find dataset:' + str(dataset))
     return x, y, testx, testy
